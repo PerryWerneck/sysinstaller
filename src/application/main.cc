c897c43eff247c79ff895e25a62b392b52061f42
Perry Werneck <perry.werneck@gmail.com>
2026-09-16 22:11:54 -0300

    Starting refactor for the new libudjat & widgets library.

--- src/application/main.cc
+++ src/application/main.cc
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: LGPL-3.0-or-later */
 
 /*
- * Copyright (C) 2025 Perry Werneck <perry.werneck@gmail.com>
+ * Copyright (C) 2026 Perry Werneck <perry.werneck@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
@@ -21,78 +21,29 @@
   * @brief Application entry point.
   */
 
- #include <config.h>
-
- #include <udjat/tools/commandlineparser.h>
- #include <udjat/tools/intl.h>
- #include <udjat/tools/logger.h>
- #include <udjat/tools/configuration.h>
- #include <udjat/tools/intl.h>
- #include <udjat/tools/string.h>
- #include <udjat/tools/url.h>
- #include <reinstall/tools/kernelparameter.h>
- #include <reinstall/tools/repository.h>
- #include <reinstall/tools/writer.h>
- #include <reinstall/dialog.h>
- #include <reinstall/application.h>
- #include <private/console.h>
- #include <udjat/tools/quark.h>
- #include <locale.h>
- 
  #ifdef HAVE_GTKMM
- #include <gtkmm.h>
- #include <private/toplevel.h>
+	#include <gtkmm.h>
  #endif
+ 
+ #include <config.h>
+ #include <udjat/tools/logger.h>
+ #include <udjat/tools/argumentparser.h>
+ #include <udjat/tools/configuration.h>
 
- using namespace std;
  using namespace Udjat;
 
+ int main(int argc, char **argv) {
+
 #ifdef HAVE_GTKMM
- static bool tui_only() noexcept {
-
-#ifndef _WIN32
-	const char *session_type = getenv("XDG_SESSION_TYPE");
-	if(session_type) {
-		Logger::String{"Detected '",session_type,"' session"}.trace();
-		return session_type[0] == 0;
-	}
-#endif // _WIN32
-
-		return false;
- }
+	enum {
+		TEXT_MODE,
+		GRAPHICAL_MODE,
+	} mode = GRAPHICAL_MODE;
+#else
+	enum {
+		TEXT_MODE,
+	} mode = TEXT_MODE;
 #endif // HAVE_GTKMM
-
- int main(int argc, char* argv[]) {
-
-	// Check for help options.
-	static const Udjat::CommandLineParser::Argument options[] = {
-#ifdef HAVE_GTKMM
-		{ 't', "tui", _( "Use text user interface" ) },
-		{ 'g', "gui", _( "Use graphic user interface" ) },
-#endif // HAVE_GTKMM
-		{ 'k', "kernel-parameter=n=v", _( "Set kernel parameter 'n' to 'v'" ) },
-		{ 'O', "output=img", _( "Write resulting image to file 'img' instead of usb" ) },
-		{ 'i', "install=url", _("Set install url, disable slp") },
-		{ 'r', "repo=r=u", _("Set url for repository 'r' to 'u', disable slp") },
-		{ 'y', "non-interactive", _("Non interactive") },
-		{ 'Q', "quit", _("Quit after processing") },
-		{ 'R', "reboot", _("Reboot on success") },
-		{ 'S', "select=option", _( "Auto select option" ) },
-		{ 'T', "target=target", _( "Auto select target" ) },
-		{ }
-	};
-
-	bool non_interactive = false;
-	{
-		const char *ptr = strrchr(argv[0],'/');
-		if(ptr) {
-			ptr++;
-			non_interactive = (strcmp(ptr,"reinstall-enable") == 0 || strcmp(ptr,"reinstall-system") == 0);
-		}
-	}
-
-	// Initialize the application.
-	Quark::init();
 
 #ifdef DEBUG
 	Udjat::Config::allow_user_homedir(true);
@@ -100,138 +51,18 @@
 	Logger::console(true);
 #endif
 
-	if(Udjat::CommandLineParser::options(argc,argv,options)) {
-		return 0;
-	}
-
-	Logger::redirect();
-
-	// Parse command line options.
-	{
-		CommandLineParser options(argc,argv);
-		String value;
-
-		if(options.get_argument(argc,argv,'i',"install",value)) {
-			Reinstall::Repository::preset("install",value.c_str());	
-		}
-
-		if(options.get_argument(argc,argv,'O',"output",value)) {
-			Reinstall::Writer::set_output(value.c_str());
-		}
-
-		if(options.get_argument(argc,argv,'S',"select",value)) {
-			Reinstall::Action::preset(value.c_str());
-		}
-
-		if(options.get_argument(argc,argv,'S',"select",value)) {
-			Reinstall::Action::preset(value.c_str());
-		}
-
-		if(options.get_argument(argc,argv,'T',"target",value)) {
-
-			auto targets = value.split(",",2);
-
-			if(targets.empty() || targets[0].empty()) {
-				throw std::runtime_error{Logger::Message(_("Invalid target '{}', please check your configuration"),value)};
-			}
-
-			//
-			// Get TARGET URL
-			//
-#ifdef DEFAULT_INSTALL_URL
-			URL target{Config::Value<string>{"install-targets",targets[0].c_str(),DEFAULT_INSTALL_URL}.c_str()};
-#else
-			URL target{Config::Value<string>{"install-targets",targets[0].c_str(),""}.c_str()};
-			if(target.empty()) {
-				target = Config::Value<string>{"install-targets","default"}.c_str();
-			}
-#endif // DEFAULT_INSTALL_URL
-
-			if(target.empty()) {
-				throw std::runtime_error{Logger::Message(_("No target found for '{}', please check your configuration"),targets[0])};
-			}
-
-			target.expand([&targets](const char *name, std::string &val) -> bool{
-
-				if(strcasecmp(name,"hostname") == 0 || strcasecmp(name,"ip") == 0 || strcasecmp(name,"server") == 0) {
-					if(targets.size() < 2) {
-						throw std::runtime_error{"This target requires a second argument, please check your configuration"};
-					}
-					val = targets[1].c_str();
-					return true;
-
-				} else if(strcasecmp(name,"target") == 0) {
-					val = targets[0].c_str();
-					return true;
-				}
-				
-				return false;
-			});
-
-			if(targets.size() > 1) {
-				target.hostname(targets[1].c_str());
-			}
-
-			Logger::String{"Using non interactive mode with target '",target.c_str(),"'"}.info();
-
-			Reinstall::Dialog::preset(Reinstall::Dialog::NonInteractive);
-			Reinstall::Repository::preset("install",target.c_str());	
-
-		}
-
-		if(options.has_argument(argc,argv,'R',"reboot")) {
-			Reinstall::Dialog::preset(Reinstall::Dialog::NonInteractiveReboot);
-		}
-
-		if(options.has_argument(argc,argv,'y',"non-interactive") || non_interactive) {
-			Reinstall::Dialog::preset(Reinstall::Dialog::NonInteractive);
-		}
-
-		while(options.get_argument(argc,argv,'r',"repo",value)) {
-			auto args = Udjat::String{value.c_str()}.split("=",2);
-			if(args.size() != 2) {
-				throw std::runtime_error{Logger::Message(_("Invalid repository argument: {}"),value)};
-			}
-			Reinstall::Repository::preset(args[0].c_str(),args[1].c_str());
-		}
-
-		while(options.get_argument(argc,argv,'k',"kernel-parameter",value)) {
-			Reinstall::KernelParameter::preset(value.c_str());
-		}
-
-	}
-
 
 #ifdef HAVE_GTKMM
-	// If the user requested TUI, run as a TUI application.
-
-	if( !CommandLineParser::has_argument(argc,argv,'g',"gui") && (CommandLineParser::has_argument(argc,argv,'t',"tui") || tui_only())) {
-
-		// Run as a TUI application.
-		return Reinstall::Console{}.run(argc,argv);
-
-	} else {
-
-		// Run as a GUI application.
-		if(Reinstall::Dialog::has_preset(Reinstall::Dialog::NonInteractive)) {
-
-			return Gtk::Application::create(G_STRINGIFY(PACKAGE_DOMAIN) "." PACKAGE_NAME)->make_window_and_run<NonInteractiveWindow>(1,argv);
-
-		} else {
-
-			return Gtk::Application::create(G_STRINGIFY(PACKAGE_DOMAIN) "." PACKAGE_NAME)->make_window_and_run<InteractiveWindow>(1,argv);
-		
-		}
-
-	}
-#else 
-	{
-		// No GTK, run as TUI application
-		return Reinstall::Console{}.run(argc,argv);
-
+	if(mode == GRAPHICAL_MODE) {
+		Logger::String{"starting graphical mode"}.trace();
+		return 0;
 	}
 #endif // HAVE_GTKMM
 
+	Logger::String{"Starting text mode"}.trace();
+	
+
 	return 0;
  }
+
 
